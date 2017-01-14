dceecf17285ab4008f3c77f45080bdd41baaf317
USTC-Tim <tim-he@qq.com>
2017-01-14 16:08:49 +0800

    add extract_count

--- tianchi-weibo-pred/read_data.py
+++ tianchi-weibo-pred/read_data.py
@@ -22,5 +22,24 @@ def read_data(data_path):
     weibo_train_data.close()
     return data
 
+def extract_count(data):
+    """
+    extract forward_count, comment_count, like_count from data and save as list(float).
+    """
+    forward_count_data = list()
+    comment_count_data = list()
+    like_count_data = list()
+    for element in data:
+        forward_count_data.append(float(element[4]))
+        comment_count_data.append(float(element[5]))
+        like_count_data.append(float(element[6]))
+    return forward_count_data, comment_count_data, like_count_data
+
+
+
 if __name__ == '__main__':
     TRAIN_DATA = read_data('../Weibo_Data/weibo_train_data.txt')
+    FORWARD, COMMENT, LIKE = extract_count(TRAIN_DATA)
+    print sum(FORWARD) / len(FORWARD)
+    print sum(COMMENT) / len(COMMENT)
+    print sum(LIKE) / len(LIKE)
